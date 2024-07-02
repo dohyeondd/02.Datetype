844662e5ede49d8efcb7fb5326c05dede7e69e26
dohyeondd <parkdohyeon1235@gmail.com>
2024-07-02 17:20:33 +0900

    quiz
    과제

---
+++ quiz.py
@@ -0,0 +1,19 @@
+name =input('이름을입력하세요')
+age = input('나이를입력하세요')
+email=input('이메일을 입력하세요')
+number=input('연락처를 입력하세요')
+
+
+dic ={name: { age, email, number}}
+print(dic)
+
+#2
+exchange = {'달러': 1320, '엔화' :800, '위안':130}
+chul= [13,200,13]
+
+ chul = [13,200,13]
+
+ print(exchange['달러']*chu[0]+exchange['엔화']*chul[1])
+
+
+              
