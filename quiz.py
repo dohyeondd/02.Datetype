2e9bd5663a249abbff54c02a070525791f9ee872
dohyeondd <parkdohyeon1235@gmail.com>
2024-07-02 17:30:55 +0900

    고제
    12

--- quiz.py
+++ quiz.py
@@ -1,19 +1,27 @@
-name =input('이름을입력하세요')
-age = input('나이를입력하세요')
-email=input('이메일을 입력하세요')
-number=input('연락처를 입력하세요')
+name1 =input('이름을입력하세요')
+age1 = input('나이를입력하세요')
+email1=input('이메일을 입력하세요')
+number1=input('연락처를 입력하세요')
 
+name2 =input('이름을입력하세요')
+age2 = input('나이를입력하세요')
+email2=input('이메일을 입력하세요')
+number2=input('연락처를 입력하세요')
 
-dic ={name: { age, email, number}}
-print(dic)
+dic1 ={name1: [age1, email1, number1]}
+dic2 ={name2: [age2, email2, number2]}
+
+print(dic1)
+print(dic2)
+
 
 #2
 exchange = {'달러': 1320, '엔화' :800, '위안':130}
 chul= [13,200,13]
 
- chul = [13,200,13]
+chul = [13,200,13]
 
- print(exchange['달러']*chu[0]+exchange['엔화']*chul[1])
+total=exchange['달러']*chul[0]+exchange['엔화']*chul[1]
+print('가지고 계신 돈은 '+ str(total) +'원 입니다.')
 
 
-              
